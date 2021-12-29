6c8dd0029ff747e3280b6cc5faad7ea3a1c1b9ff
Muhammad-Adil <adil.muhammad93@gmail.com>
2021-12-29 15:27:02 +0100

    Edit Tshirt.json File for getting dummy data

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import Navbar from './components/Navbar';
+import Header from './components/Header';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Reports from './pages/Reports';
+import Articles from './pages/Articles';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <div className="test-app">
+      <Router>
+        <Navbar />
+        <div className="site-main">
+          <Header />
+          {/* <Articles/>
+         */}
+          <Switch>
+            <Route path='/' exact component={Home} />
+            <Route path='/reports' component={Reports} />
+            <Route path='/articles' component={Articles} />
+          </Switch>
+        </div>
+      </Router>
+      </div>
+    </>
   );
 }
 
