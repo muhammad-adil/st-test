128ac28149722513f3f3de7f45635f34525b52aa
Muhammad-Adil <adil.muhammad93@gmail.com>
2022-01-04 13:21:04 +0100

    Card and cardStatsDetails Components  updated

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,74 +1,207 @@
-import React, { useState } from 'react';
-import { FaRegPlayCircle } from 'react-icons/fa';
-import CardStatsDetails from './CardStatsDetails.js';
-import ActionBox from './ActionBox.js';
+import React, { useState } from "react";
+import {
+  FaRegPlayCircle,
+  FaRegPauseCircle,
+  FaRegStopCircle
+} from "react-icons/fa";
+import CardStatsDetails from "./CardStatsDetails.js";
+import ActionBox from "./ActionBox.js";
 //1st way
-import tshirtData from '../data/tshirt.json';
-import './Card.css';
+import tshirtData from "../data/tshirt.json";
+import "./Card.css";
 
-export default function Card() {
-  const [data, setData] = useState(tshirtData)
+export default function Card(props) {
+  const [data, setData] = useState(tshirtData);
+  console.log(props, "props in Cardjs");
+  console.table(data);
 
-  // console.log(data, "data from state")
+  // const [isActive, setActive] = useState(false);
+  // const toggleActiveClass = () => {
+  //   props.setActive(!props.isActive);
+  //   // props.setActive(props.isActive ? true : false); // testing
+  //   // props.setActive(props.isActive ? false : true);
+  //   console.log(
+  //     props.setActive,
+  //     "toggleActiveClass Fired in Card.js",
+  //     props.isActive
+  //   );
+  // };
 
   if (data) {
-    // render() {
-    return <div className="container d-flex f-wrap">{data && data.map((tshirt) => <CardItem tshirt={tshirt} />)}</div>
-  }
-}
-
-function CardItem({ tshirt }) {
-  const [show, setShow] = useState(false)
-
-  const handleChange = () => {
-    setShow(!show)
-  }
-
-  return (
-    <div key={tshirt.id} className="cards">
-      <div className="card-main" onClick={handleChange}>
-        <div className="grid-container-top">
-          <div className="play-btn text-align-left">
-            <FaRegPlayCircle />
-          </div>
-          <div className="card-heading text-align-center">
-            <p>
-              T-SHIRT
-              <br />
-              {tshirt.season}
-            </p>
-          </div>
-          <div className="card-status text-align-right">
-            <span className="dot-green"></span>
-          </div>
-        </div>
-        {show ? (
-          <ActionBox />
-        ) : (
-          <CardStatsDetails
-            tshirt_id={tshirt.tshirt_id}
-            tshirt_img={tshirt.tshirt_img}
-            wear_type={tshirt.wear_type}
-            we={tshirt.we}
-            actions={tshirt.actions}
-            sales_rate={tshirt.sales_rate}
-            first_sales={tshirt.first_sales}
-            items_per_week={tshirt.items_per_week}
-            return_rate={tshirt.return_rate}
-            sale_price={tshirt.sale_price}
-            sale_currency={tshirt.sale_currency}
-            preorder={tshirt.preorder}
-            reorder={tshirt.reorder}
-            rearrangement={tshirt.rearrangement}
-            delivered={tshirt.delivered}
-            returned={tshirt.returned}
-            sold={tshirt.sold}
-            correction={tshirt.correction}
-            stock={tshirt.stock}
-            handleChange={handleChange}
-          />
-        )}
+    // && props
+    return (
+      <div className="cards-container d-flex f-wrap">
+        {data &&
+          data.map((tshirt) => (
+            <CardItem
+              tshirt={tshirt}
+              handleChangeShowActionCheckBox={() =>
+                props.handleChangeShowActionCheckBox
+              }
+              toggleActiveClass={() => props.toggleActiveClass}
+              setShowActionCheckBox={props.setShowActionCheckBox}
+              isActive={props.isActive}
+              setActive={props.setActive}
+            />
+            // showActionCheckBox={props.showActionCheckBox}
+          ))}
       </div>
-    </div>
-  )
+    );
+  }
+  function CardItem({ tshirt, ...props }) {
+    console.log(props, "props in cardItem");
+
+    const [show, setShow] = useState(false);
+    const handleChange = () => {
+      setShow(!show);
+    };
+
+    return (
+      <div key={tshirt.id} className="cards">
+        <div
+          className={`card-main ${props.isActive ? "card-main-active" : ""}`}
+          onClick={handleChange}
+          // onClick={() => {
+          //   handleChange();
+          //   props.toggleActiveClass();
+          // }}
+        >
+          <div className="grid-container-top">
+            {/* <div className="play-btn text-align-left"> */}
+            {tshirt.status === "open" && (
+              <div className="play-btn text-align-left">
+                <FaRegPlayCircle />
+              </div>
+              // <span className="dot-green-15"></span>
+            )}
+            {tshirt.status === "pause" && (
+              <div className="pause-btn text-align-left">
+                <FaRegPauseCircle />
+              </div>
+              // <span className="dot-orange-15"></span>
+            )}
+            {tshirt.status === "stop" && (
+              <div className="stop-btn text-align-left">
+                <FaRegStopCircle />
+              </div>
+              // <span className="dot-red-15"></span>
+            )}
+            {/* <FaRegPlayCircle /> */}
+            {/* </div> */}
+            <div className="card-heading text-align-center">
+              <p>
+                T-SHIRT
+                <br />
+                {tshirt.season}
+              </p>
+            </div>
+            <div className="card-status text-align-right">
+              <span className={`dot-green ${tshirt.status_color}`}></span>
+            </div>
+          </div>
+          {show ? (
+            <ActionBox />
+          ) : (
+            <CardStatsDetails
+              tshirt_id={tshirt.tshirt_id}
+              tshirt_img={tshirt.tshirt_img}
+              wear_type={tshirt.wear_type}
+              we={tshirt.we}
+              actions={tshirt.actions}
+              sales_rate={tshirt.sales_rate}
+              first_sales={tshirt.first_sales}
+              items_per_week={tshirt.items_per_week}
+              return_rate={tshirt.return_rate}
+              sale_price={tshirt.sale_price}
+              sale_currency={tshirt.sale_currency}
+              preorder={tshirt.preorder}
+              reorder={tshirt.reorder}
+              rearrangement={tshirt.rearrangement}
+              delivered={tshirt.delivered}
+              returned={tshirt.returned}
+              sold={tshirt.sold}
+              correction={tshirt.correction}
+              stock={tshirt.stock}
+              status_color={tshirt.status_color}
+              handleChange={handleChange}
+            />
+          )}
+          {/* <div className="action-box-overlay">
+            <form>
+              <label className="action-box-checkbox">
+                Select item
+                <input className="checkbox" type="checkbox" />
+                <span className="checkmark"></span>
+              </label>
+            </form>
+          </div> */}
+
+          {props.setShowActionCheckBox ? (
+            <div className="action-box-overlay">
+              <form>
+                <label className="action-box-checkbox">
+                  Select item
+                  <input
+                    className="checkbox"
+                    type="checkbox"
+                    onChange={() => props.toggleActiveClass()} //props.toggleActiveClass
+                    // onClick={() => props.toggleActiveClass()}
+                    checked={props.isActive}
+                  />
+                  <span className="checkmark"></span>
+                </label>
+              </form>
+            </div>
+          ) : null}
+        </div>
+      </div>
+    );
+  }
+
+  // function CardAction(props) {
+  //   console.log(props, "props in CardActionBox");
+  //   const toggleActiveClass = () => {
+  //     props.setActive(!props.isActive);
+  //     console.log(
+  //       props.settActive,
+  //       "toggleActiveClass Fired in CardActionBox",
+  //       props.isActive
+  //     );
+  //   };
+  //   return (
+  //     <div>
+  //       {data.map((t) => (
+  //         <CardAction
+  //           setShowActionCheckBox={props.setShowActionCheckBox}
+  //           isActive={props.isActive}
+  //           setActive={props.setActive}
+  //         />
+  //       ))}
+  //     </div>
+  //   );
+
+  //   function CardAction(props) {
+  //     console.log(props, "props in cardActionBox");
+  //     return (
+  //       <div className="">
+  //         {props.setShowActionCheckBox ? (
+  //           <div className="action-box-overlay">
+  //             <form>
+  //               <label className="action-box-checkbox">
+  //                 Select item
+  //                 <input
+  //                   className="checkbox"
+  //                   type="checkbox"
+  //                   onChange={() => toggleActiveClass()}
+  //                   checked={props.setActive}
+  //                 />
+  //                 <span className="checkmark"></span>
+  //               </label>
+  //             </form>
+  //           </div>
+  //         ) : null}
+  //       </div>
+  //     );
+  //   }
+  // }
 }
--- src/components/CardStatsDetails.js
+++ src/components/CardStatsDetails.js
@@ -1,98 +1,138 @@
-import React from "react";
-import './Card.css';
+import React, { useState } from "react";
+import "./Card.css";
 // import tshirtData from "../data/tshirt.json";
 
 export default function CardStatsDetails(props) {
-    // console.log(props, "props from cardStats")
-    // console.log(props.tshirt_id, "props tshirt_id")
-    // console.log(props.wear_type, "props weartype")
-    // console.log(props.tshirts, "props-tshirts from cardStats")
-    // if (props && props) {
-    return (
-                        <div className="grid-container" key={Math.random().toString(36).substr(2, 9)}>
-                            <div className="tshirt-img">
-                                <img src={props.tshirt_img} alt="" />
-                                <div className="tshirt_id">
-                                    <p>{props.wear_type} - {props.tshirt_id}</p>
-                                </div>
-                            </div>
+  // const [showActionCheckBox, setShowActionCheckBox] = useState(true);
+  // console.log(props, "props from cardStats");
 
-                            <table className="card-table">
-                                <thead>
-                                    <tr>
-                                        <th>WE:{props.we}</th>
-                                        <th className="mr-11">Actions:</th>
-                                        <th>{props.actions}</th>
-                                    </tr>
-                                </thead>
-                                <tbody>
-                                    <tr>
-                                        <td>Sales rate</td>
-                                        <td className="align-right mr-11">{props.sales_rate}</td>
-                                        <td><span className="dot-green-15"></span></td>
-                                    </tr>
-                                    <tr>
-                                        <td>1st Sales</td>
-                                        <td className="align-right mr-11">{props.first_sales}</td>
-                                        <td><span className="dot-green-15"></span></td>
-                                    </tr>
-                                    <tr>
-                                        <td>Items per week</td>
-                                        <td className="align-right mr-11">{props.items_per_week}</td>
-                                        <td><span className="dot-green-15"></span></td>
-                                    </tr>
-                                    <tr>
-                                        <td>Return rate</td>
-                                        <td className="align-right mr-11">{props.return_rate}</td>
-                                        <td><span className="dot-green-15"></span></td>
-                                    </tr>
-                                    <tr>
-                                        <td>Sale Price</td>
-                                        <td className="align-right">{props.sale_price}&nbsp;</td>
-                                        <td>{props.sale_currency}</td>
-                                    </tr>
-                                </tbody>
-                            </table>
+  return (
+    <div>
+      <div
+        className="grid-container"
+        key={Math.random().toString(36).substr(2, 9)}
+      >
+        <div className="tshirt-img">
+          <img src={props.tshirt_img} alt="" />
+          <div className="tshirt_id">
+            <p>
+              {props.wear_type} - {props.tshirt_id}
+            </p>
+          </div>
+        </div>
 
-                            <div className="grid__container-bottom">
-                                <div className="stat__card">
-                                    <div className="stat__container">
-                                    <div className="stat__title">Preorder</div>
-                                    <div className="stat__content">{props.preorder}</div>
-                                    </div>
-                                    <div className="stat__container">
-                                    <div className="stat__title">Reorder</div>
-                                    <div className="stat__content">{props.reorder}</div>
-                                    </div>
-                                    <div className="stat__container">
-                                    <div className="stat__title">Rearrangemt.</div>
-                                    <div className="stat__content">{props.rearrangement}</div>
-                                    </div>
-                                    <div className="stat__container">
-                                    <div className="stat__title">Delivered</div>
-                                    <div className="stat__content">{props.delivered}</div>
-                                    </div>
-                                    <div className="stat__container">
-                                    <div className="stat__title">Returned</div>
-                                    <div className="stat__content">{props.returned}</div>
-                                    </div>
-                                    <div className="stat__container">
-                                    <div className="stat__title">Sold</div>
-                                    <div className="stat__content">{props.sold}</div>
-                                    </div>
-                                    <div className="stat__container">
-                                    <div className="stat__title">Correction</div>
-                                    <div className="stat__content">{props.correction}</div>
-                                    </div>
-                                    <div className="stat__container">
-                                    <div className="stat__title">Stock</div>
-                                    <div className="stat__content">{props.stock}</div>
-                                    </div>
-                                </div>
+        <table className="card-table">
+          <thead>
+            <tr>
+              <th>WE:{props.we}</th>
+              <th className="mr-11">Actions:</th>
+              <th>{props.actions}</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>Sales rate</td>
+              <td className="align-right mr-11">{props.sales_rate}</td>
+              <td>
+                <span className={`dot-${props.status_color}-15`}></span>
+                {/* <span className="dot-green-15"></span> */}
+                {/* <span className="dot-green-15"></span> */}
+              </td>
+            </tr>
+            <tr>
+              <td>1st Sales</td>
+              <td className="align-right mr-11">{props.first_sales}</td>
+              <td>
+                <span className="dot-green-15"></span>
+                {/* {props.status === "green" ? (
+                  <span className="dot-green-15"></span>
+                ) : null}
+                {props.status === "orange" ? (
+                  <span className="dot-orange-15"></span>
+                ) : null}
+                {props.status === "red" ? (
+                  <span className="dot-red-15"></span>
+                ) : null} */}
+                {/* {props.status === "open" && (
+                  <span className="dot-green-15"></span>
+                )}
+                {props.status === "pause" && (
+                  <span className="dot-orange-15"></span>
+                )}
+                {props.status === "stop" && (
+                  <span className="dot-red-15"></span>
+                )} */}
 
-                            </div>
+                {/* {props.status === "open" ? (
+                  <span className="dot-green-15"></span>
+                ) : props.status === "pause" ? (
+                  <span className="dot-orange-15"></span>
+                ) : props.status === "stop" ? (
+                  <span className="dot-red-15"></span>
+                ) : (
+                  <span className={`dot-${props.status_color}-15`}></span>
+                )} */}
+              </td>
+            </tr>
+            <tr>
+              <td>Items per week</td>
+              <td className="align-right mr-11">{props.items_per_week}</td>
+              <td>
+                <span className="dot-green-15"></span>
+              </td>
+            </tr>
+            <tr>
+              <td>Return rate</td>
+              <td className="align-right mr-11">{props.return_rate}</td>
+              <td>
+                <span className="dot-green-15"></span>
+              </td>
+            </tr>
+            <tr>
+              <td>Sale Price</td>
+              <td className="align-right">{props.sale_price}&nbsp;</td>
+              <td>{props.sale_currency}</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
 
-
-                        </div>
-    )
+      <div className="grid__container-bottom">
+        <div className="stat__card">
+          <div className="stat__container">
+            <div className="stat__title">Preorder</div>
+            <div className="stat__content">{props.preorder}</div>
+          </div>
+          <div className="stat__container">
+            <div className="stat__title">Reorder</div>
+            <div className="stat__content">{props.reorder}</div>
+          </div>
+          <div className="stat__container">
+            <div className="stat__title">Rearrangemt.</div>
+            <div className="stat__content">{props.rearrangement}</div>
+          </div>
+          <div className="stat__container">
+            <div className="stat__title">Delivered</div>
+            <div className="stat__content">{props.delivered}</div>
+          </div>
+          <div className="stat__container">
+            <div className="stat__title">Returned</div>
+            <div className="stat__content">{props.returned}</div>
+          </div>
+          <div className="stat__container">
+            <div className="stat__title">Sold</div>
+            <div className="stat__content">{props.sold}</div>
+          </div>
+          <div className="stat__container">
+            <div className="stat__title">Correction</div>
+            <div className="stat__content">{props.correction}</div>
+          </div>
+          <div className="stat__container">
+            <div className="stat__title">Stock</div>
+            <div className="stat__content">{props.stock}</div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
