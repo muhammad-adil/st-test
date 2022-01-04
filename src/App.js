4a2fd725d6024b067e1e28f8de238d85a6750700
Muhammad-Adil <adil.muhammad93@gmail.com>
2022-01-04 13:37:54 +0100

    ActionBox.css and Navbar.css  updated

--- src/App.js
+++ src/App.js
@@ -1,29 +1,28 @@
-import React from 'react';
-import './App.css';
-import Navbar from './components/Navbar';
-import Header from './components/Header';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import Home from './pages/Home';
-import Reports from './pages/Reports';
-import Articles from './pages/Articles';
+import React from "react";
+import "./App.css";
+import Navbar from "./components/Navbar";
+import Header from "./components/Header";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Reports from "./pages/Reports";
+import Articles from "./pages/Articles";
 
 function App() {
   return (
     <>
-    <div className="test-app">
-      <Router>
-        <Navbar />
-        <div className="site-main">
-          <Header />
-          {/* <Articles/>
-         */}
-          <Switch>
-            <Route path='/' exact component={Home} />
-            <Route path='/reports' component={Reports} />
-            <Route path='/articles' component={Articles} />
-          </Switch>
-        </div>
-      </Router>
+      <div className="wrapper">
+        <Router>
+          <Navbar />
+          <div className="page-content">
+            <Header />
+
+            <Switch>
+              <Route path="/" exact component={Home} />
+              <Route path="/reports" component={Reports} />
+              <Route path="/articles" component={Articles} />
+            </Switch>
+          </div>
+        </Router>
       </div>
     </>
   );
