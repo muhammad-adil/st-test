266a102a26655105ecbcbf1fd182d41ca069694d
Muhammad-Adil <adil.muhammad93@gmail.com>
2022-01-04 13:19:11 +0100

    ActionHeader.js updated

--- src/components/ActionHeader.js
+++ src/components/ActionHeader.js
@@ -1,20 +1,69 @@
-import React from 'react'
-import {HiOutlineLightningBolt} from 'react-icons/hi';
-import './ActionHeader.css';
-import { FaRegPlayCircle } from 'react-icons/fa';
-export default function ActionHeader() {
-    return (
-    <div className="container">
-    <div className="action-header">
+import React, { useState } from "react";
+import { HiOutlineLightningBolt } from "react-icons/hi";
+import "./ActionHeader.css";
+import { FaRegPlayCircle } from "react-icons/fa";
+export default function ActionHeader(props) {
+  const [showBtn, setShowBtn] = useState(false);
+
+  const handleChangeBtn = () => {
+    setShowBtn(!showBtn);
+  };
+  console.log(props, "props in ActionHeaderjs");
+
+  return (
+    <div className="action-header-container">
+      <div className="action-header">
         <div className="btn-action">
-        <button><i><HiOutlineLightningBolt/></i>Start Action</button>
+          {showBtn ? (
+            <div>
+              <button
+                className="btn-red"
+                onClick={() => {
+                  handleChangeBtn();
+                  props.handleChangeShowActionCheckBox();
+                }}
+              >
+                <i>
+                  <HiOutlineLightningBolt />
+                </i>
+                Stop Action
+              </button>
+              <form>
+                <label className="action-box-checkbox">
+                  Select All
+                  <input
+                    className="checkbox"
+                    type="checkbox"
+                    checked={props.isActive}
+                    onChange={() => props.toggleActiveClass()}
+                  />
+                  <span className="checkmark"></span>
+                </label>
+              </form>
+            </div>
+          ) : (
+            <button
+              onClick={() => {
+                handleChangeBtn();
+                props.handleChangeShowActionCheckBox();
+              }}
+            >
+              <i>
+                <HiOutlineLightningBolt />
+              </i>
+              Start Action
+            </button>
+          )}
+          {/* <button><i><HiOutlineLightningBolt/></i>Start Action</button> */}
         </div>
 
         <div className="total-items">
-        <p>223 items</p>
+          <p>223 items</p>
         </div>
-        <div className="action-status play-btn"><FaRegPlayCircle /></div>
+        <div className="action-status play-btn">
+          <FaRegPlayCircle />
+        </div>
+      </div>
     </div>
-    </div>
-    )
+  );
 }
